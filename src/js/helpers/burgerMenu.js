5c980f2ca06a353b3e76670d0a476255f53cad95
Dmitriy <akselmega95@gmail.com>
2021-11-01 13:32:47 +0200

    scroll btn add

--- src/js/helpers/burgerMenu.js
+++ src/js/helpers/burgerMenu.js
@@ -1,7 +1,30 @@
-const burger = document.querySelector('.home-header__btn-menu');
-const navMenu = document.querySelector('.home-header__burger-menu-nav')
+const btnMenu = document.querySelector('.home-header__btn-menu');
+const burgerMenuNav = document.querySelector('.home-header__burger-menu-nav')
 
-	burger.addEventListener('click', () => {
-		burger.classList.toggle('home-header__btn-menu_active');
-		navMenu.classList.toggle('home-header__burger-menu-nav_active');
-})
+btnMenu.addEventListener('click', () => {
+	btnMenu.classList.toggle('home-header__btn-menu_active');
+	burgerMenuNav.classList.toggle('home-header__burger-menu-nav_active');
+});
+
+
+
+const btnToTop = document.querySelector('.home-main__btn-to-top');
+
+window.addEventListener('scroll', checkScroll);
+btnToTop.addEventListener('click', backToTop);
+
+function checkScroll() {
+	const scrolled = window.scrollY;
+	if (scrolled > 100) {
+		btnToTop.classList.add('home-main__btn-to-top_show');
+	}	else {
+		btnToTop.classList.remove('home-main__btn-to-top_show');
+	}
+}
+
+function backToTop() {
+	if (window.scrollY > 0) {
+		window.scrollBy(0, -20);
+		setTimeout(backToTop, 0);
+	}
+}
